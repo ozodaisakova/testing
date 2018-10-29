9ec477b8fb82ac50721b598e393ce96e754e14df
Ozoda Isakova <ozoda724@gmail.com>
2018-10-29 19:20:27 +0600

    Add images as static file

--- pages/index.vue
+++ pages/index.vue
@@ -29,16 +29,16 @@
       return {
         items: [
           {
-            src: 'https://ch3301files.storage.live.com/y4piQixBo51zLy_mTkN6UZ0_kPLewtSsQ1SWFOAOnWc3ciBMmUVGV82fAhyS7qXwTzQ_jFHGUlqMBy5SPf0hhZ44TtOdUEkBCIjaTNvdPAecaUIxjQGp3sln63Op1eft69JojfoTjbIJ3yhVaQGPsod-lKChx9ylcFeIa0leoV3dG1FE1HWcvI9es42VSMUxC4-/IMG_20160925_230333_603.jpg?psid=1&width=494&height=617'
+            src: '/r-1.jpg'
           },
           {
-            src: 'https://ch3302files.storage.live.com/y4p_u7HO3uyzAfXJaulJXwKq34RZM-m07WBr8FBD_0SM8CVxp62oxKiYANEXThUn2PAFxdM43kTA9zIje3X4sLke-TpvWcTCYfZXZCi_s080PceLfD3kWDTqeSc1Uq_KU9VQ_nv3lAfslUF-CA-2uJ87_cXurvjxIVNa-tUJ8jyuVvO8C_n-g3UU8_N_powqbq_/00mQRcZJlNo.jpg?psid=1&width=110&height=195'
+            src: '/r-2.jpg'
           },
           {
-            src: 'https://ch3302files.storage.live.com/y4p4YK-sBcio1BRYzdKyaauUfn0dfqxl7l6v73Rw3iLFnhhcT-8xisryV_1t2NiB2M1rf7wetYaqejgKBVNZkqSCowgSVSiUd4KOnuUu7H0L0nnlArsm5ce-4PZ4hQdHCh421E7Sk6G9cDLoJapw2_n1UoE0LUqArp2N21HuWD0NGaRzY6hP6EsqaE7Wjs1hKIB/PicsArt_1459262027427.jpg?psid=1&width=117&height=208'
+            src: '/r-3.jpg'
           },
           {
-            src: 'https://ch3301files.storage.live.com/y4p8T6iucUXtHVQjMkQqBTqxtv9sW_Smj22dY20pJo_JrS7gaNN71sl-UHjhdl88I1rxrpFotiqMnoRn8fszBhTFrMnPg3Ca5XbN6c8tysbtoBmO9mu0zL6vP0F8DJvhVIqf5JX3HnRLh98uhjrDSFJt5QT41ChWhHfO5h2ga8FCZYfRgQMgF7QjphoY0mLR62z/IMG_20151124_010006.JPG?psid=1&width=480&height=473'
+            src: '/r-4.jpg'
           }
         ]
       }
--- pages/inspire.vue
+++ pages/inspire.vue
@@ -29,16 +29,13 @@
       return {
         items: [
           {
-            src: 'https://db3pap001files.storage.live.com/y4p8rwhEJKVg1eYXMFumv3MOqQG7UAOt-Vsk7K6fgkKhw6jRy_Osc4tFtMz6aaLl9jLBJIt7ne9Gz1-9rwP2rb1nBFf_QVS6p52XWQDNXNCa17P-CMowqUzVvQMSDqBgQesRyVNJxcB2OfB6kB2pb7mNmA8vvWX24jMvgtY1rmGeFp8mkxtmBBjhEEWOQR3B2aF/IMG-20160122-WA0004.jpg?psid=1&width=463&height=617'
+            src: '/o-1.jpg'
           },
           {
-            src: 'https://db3pap001files.storage.live.com/y4pdIWDqxzmIMm92yWrsSvAtqOTCF3gDQtRTV7LW9MqgCSg-sQtHDTlF_nQz8Bm2CuR04Md1V4th26i-2bY_5OI77FM8W59ROjq8wsf4Y6Bx7uqaOrywTo3uBdzj7MujzhiKgxF0SjYP_N578x3E3xbZm0DtPbwzQRMRTxhOcshjOlFpegV92Af3yJlZB8PKgTg/2016-09-13-17-46-08-270.jpg?psid=1&width=463&height=617'
+            src: '/o-2.jpg'
           },
           {
-            src: 'https://db3pap001files.storage.live.com/y4p91RX5LF22v0nECijGCZEVmJH6HtDV9ae60nOxN2J0xGWQygt3967X8CzVg3ni-CcEsaxtzpKL9wIHf5PXt73LqskUxbaB-Bl_HVHuoNjoNc8m8eJNIOBwVPmM1Pw4eEt7OAmqdpZgX6m7uv-sQVPbDn3LhdQ95QCw38RzNXoKcQ_C07J00PnDaEBB0wokEMU/2016-09-18-16-47-03-199.jpg?psid=1&width=463&height=617'
-          },
-          {
-            src: 'https://db3pap001files.storage.live.com/y4ph25nlAh3VzICp6srL8QO9JGLMPnasIgUrUoH_lTYXU3j1R2wF2Vvo_UDh2dFU5nnDk_-cpOh-JOhgZxPsDlXDE7J1puRYkLxfIB2qGkxXR99hm4GR44kZT2BuT4D7ld0uN95ixLJA2YOkxxBkI7lP44qbwjXNeoNPc-r-W0mtYW6yEqpumkW1bHIXK9oo7MY/2016-11-24-16-43-50-304.jpg?psid=1&width=463&height=617'
+            src: '/o-3.jpg'
           }
         ]
       }
